16d3e31bcdc4a9dfc55da6c03d50799ae7d9a995
Electrolytful <erhanulusal@hotmail.com>
2024-02-07 16:00:30 +0000

    made changes to styling

--- src/App.jsx
+++ src/App.jsx
@@ -54,22 +54,21 @@ export default function App() {
 
   return (
     <main>
+      <ol id="players" className="highlight-player">
+        <Player
+          initialName={PLAYERS.X}
+          symbol="X"
+          isActive={activePlayer === "X"}
+          onChangeName={handlePlayerNameChange}
+        />
+        <Player
+          initialName={PLAYERS.O}
+          symbol="O"
+          isActive={activePlayer === "O"}free
+          onChangeName={handlePlayerNameChange}
+        />
+      </ol>
       <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player
-            initialName={PLAYERS.X}
-            symbol="X"
-            isActive={activePlayer === "X"}
-            onChangeName={handlePlayerNameChange}
-          />
-          <Player
-            initialName={PLAYERS.O}
-            symbol="O"
-            isActive={activePlayer === "O"}
-            onChangeName={handlePlayerNameChange}
-          />
-        </ol>
-
         {(winner || draw) && (
           <GameOver
             winner={draw ? undefined : winner}
